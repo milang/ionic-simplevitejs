b2b6ceef476257363d9b583151416174775772f7
Milan Gardian <mgardian@gmail.com>
2021-11-19 18:44:25 -0700

    Add comments and speed-up cold test

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
+// Babel configuration must stay in a separate file, because we are
+// specifying project-wide configuration here; if we kept it in "package.json",
+// it would be considered to be file-relative configuration; for more info see
+// https://babeljs.io/docs/en/config-files#project-wide-configuration
+
 module.exports = {
   presets: [
     ["@babel/preset-env", { useBuiltIns: "usage", corejs: 3 }],
