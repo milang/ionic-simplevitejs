a817e748ec553760115533ab577c338cd2e92aca
Milan Gardian <mgardian@gmail.com>
2021-11-25 10:17:12 -0700

    Jest fix that doesn't require core-js
    - Thanks JohnG (jgoz), AlexA (alexa-pd)

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+// Babel configuration must stay in a separate file. We are defining
+// a project-wide configuration here. If we kept it in "package.json",
+// it would be considered a file-relative configuration. For more info see
+// https://babeljs.io/docs/en/config-files#project-wide-configuration
+
+module.exports = {
+  presets: [
+    "@babel/preset-env",
+    ["@babel/preset-react", { runtime: "automatic" }],
+    "@babel/preset-typescript",
+  ],
+};
