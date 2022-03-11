63aee900a851334ab3dc5025a74eb15d208fdf65
Milan Gardian <mgardian@gmail.com>
2022-03-11 09:47:35 -0700

    Use Ionic 6 React initialization

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { setupIonicReact } from "@ionic/react";
+
+setupIonicReact();
 
 ReactDOM.render(
   <React.StrictMode>
