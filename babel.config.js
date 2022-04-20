3f0b7d80085e5e4891d2dbbeef9e48a9afa483f5
Milan Gardian <mgardian@gmail.com>
2022-04-19 19:57:24 -0600

    Update base configuration
    - Force UNIX newlines
    - Add editorconfig
    - Add VSCode extension recommendations
    - Use Prettier for all files (in VSCode)
    - Use latest ViteJS tsconfig (started using "references")
    - Enable css dev sourcemaps to "fix" browser console warnings

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,6 @@ module.exports = {
   presets: [
     "@babel/preset-env",
     ["@babel/preset-react", { runtime: "automatic" }],
-    "@babel/preset-typescript",
-  ],
+    "@babel/preset-typescript"
+  ]
 };
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   clearScreen: false,
   plugins: [react()],
+  css: {
+    devSourcemap: true
+  }
 });
