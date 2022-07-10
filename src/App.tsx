6477b79b7a344186a7e62f1053fe1afafac6bc3e
Milan Gardian <mgardian@gmail.com>
2022-07-09 22:21:11 -0600

    Start using "IonNav"

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,36 @@ import {
   IonApp,
   IonContent,
   IonHeader,
+  IonNav,
   IonPage,
   IonTitle,
   IonToolbar
 } from "@ionic/react";
 
+function HelloWorld(props: { one: number }): JSX.Element {
+  return (
+    <>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Hello</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <h1>World</h1>
+        <p>
+          Props: {props === undefined ? "<undefined>" : JSON.stringify(props)}
+        </p>
+      </IonContent>
+    </>
+  );
+}
+
 export function App(): JSX.Element {
   return (
     <IonApp>
       <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Hello</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent className="ion-padding">
-          <h1>World</h1>
-        </IonContent>
+        <IonNav root={HelloWorld} rootParams={{ one: 1 }} />
+        {/* <HelloWorld one={-1} /> */}
       </IonPage>
     </IonApp>
   );
